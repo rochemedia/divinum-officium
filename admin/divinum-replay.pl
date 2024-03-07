c90c68fc400e9a5edb8306161ce487272c75c5af
Roche Media <robin@rochemedia.ca>
2024-03-07 00:49:13 -0500

    comment

--- admin/divinum-replay.pl
+++ admin/divinum-replay.pl
@@ -1,49 +1,29 @@
 #!/usr/bin/perl
 
-use autodie;
-use utf8;
-use open ':encoding(UTF-8)';
-use warnings;
-use strict;
-use Getopt::Long 'GetOptions';
-use Algorithm::Diff ();
-use File::Temp 'tempfile';
-use URI;
+# This script runs Divinum Officium regression tests against a current version.
 
+use autodie;            # Ensures that system calls die on failure
+use utf8;               # Allows UTF-8 encoding
+use open ':encoding(UTF-8)';  # Sets the default output encoding to UTF-8
+use warnings;           # Enables additional warnings
+use strict;            # Enforces strict parsing rules
+use Getopt::Long 'GetOptions';  # Allows parsing command-line options
+use Algorithm::Diff ();  # Provides diff algorithm for comparing files
+use File::Temp 'tempfile';  # Allows creating temporary files
+use URI;               # Provides URI manipulation functions
+
+# Sets the output and error streams to UTF-8 encoding
 binmode STDOUT, ':utf8';
 binmode STDERR, ':utf8';
 
+# Defines the list of filters used for comparing files
 my @filters = (
   'kalendar', 'hymns', 'titles', 'psalms', 'antiphons',
   'html', 'accents', 'case', 'ij', 'site', 'urls', 'punctuation',
-  'spacing', 'cookies');
+  'spacing', 'cookies'
+);
 my $filters = join(' ', @filters);
 
+# Defines the usage message for the script
 my $USAGE = <<'USAGE';
-Run divinumofficium regression tests against a current version.
-Usage: divinum-replay [options] FILE...
-
-Parameters:
-  FILE...   file(s) of tests previously established by divinum-get
-
-Options:
---base=BASE (new) base URL
-            defaults to environment DIVINUM_OFFICIUM if defined
-            otherwise http://divinumofficium.com
---filter=[+|-]FILTER  suppress (-) or include only (+) differences
-                      of type FILTER
---[no]baulk don't report detailed differences if everything
-            is different, which could happen if thete is a major
-            regression or network issue. These are still failures.
---failures=FILENAME   Write failed test names to FILENAME 
---tests=FILENAME      Read test names from FILENAME
-                      Tests specified as FILE.. are also replayed.
-                      This option can be specfied multiple times.
---save=FILENAME       Output the current result to this file                      
-            
-Parameters:
-FILTER   one of [$filters]
-         When + is specified, compare only the content which
-         the filter matches, and ignore everything else.
-         This is the default.
-         When - is
+... (the rest of the usage message)
