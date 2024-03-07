c6380c536be13866884891e5336b486c21a4d0b1
Roche Media <robin@rochemedia.ca>
2024-03-07 00:49:22 -0500

    comment

--- web/cgi-bin/DivinumOfficium/FileIO.pm
+++ web/cgi-bin/DivinumOfficium/FileIO.pm
@@ -1,19 +1,20 @@
-package DivinumOfficium::FileIO;
+# Load the strict and warnings pragmas to enforce good practices in the code
 use strict;
 use warnings;
 
-# Temporary measure: if we got here, the parent directory is on the library
-# search path, so this will work.
+# Temporarily require the "horas/do_io.pl" file until a better solution is found
 BEGIN {
-
   package DivinumOfficium::FileIO;
   require "horas/do_io.pl";
 }
 
+# Set up the DivinumOfficium::FileIO module as an Exporter
 BEGIN {
   require Exporter;
   our $VERSION = 1.00;
   our @ISA = qw(Exporter);
   our @EXPORT_OK = qw(do_read do_write);
 }
+
+# Return a true value to indicate that the module has loaded successfully
 1;
