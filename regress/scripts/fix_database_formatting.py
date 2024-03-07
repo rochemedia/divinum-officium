4d3a13856fbf2e61033484cf454dc6b8a22f59e5
Roche Media <robin@rochemedia.ca>
2024-03-07 00:48:11 -0500

    improve

--- regress/scripts/fix_database_formatting.py
+++ regress/scripts/fix_database_formatting.py
@@ -2,55 +2,38 @@ import os
 import pathlib
 import re
 
-
-CITATION_REGEX = r"^\s*(?:!)\s?(?P<citation>(?P<book>(?:\d\s)?\w+)\s?(?P<chapter>\d+)(?::|,\s)(?P<verses>\d+-?\d*,?\d*))\s*$"
-
+CITATION_REGEX = r"^\s*(?:!)\s*(?P<book>(?:\d\s)?\w+)\s*(?P<chapter>\d+)(?::|,\s*)(?P<verses>\d+-?\d*,?\d*)\s*$"
 
 def fix_encoding(path: pathlib.Path):
     try:
-        with path.open() as f:
-            content = f.read()
+        content = path.read_text()
     except UnicodeDecodeError:
-        with path.open(encoding="windows-1252") as f:
-            content = f.read()
-        with path.open("w", encoding="utf-8") as f:
-            f.write(content)
-
+        content = path.read_text(encoding="windows-1252")
+        path.write_text(content, encoding="utf-8")
 
 def fix_bible_references(path: pathlib.Path):
     if not path.suffix == ".txt":
         return
-    with path.open() as f:
-        content = f.read()
+    content = path.read_text()
     fixed_whitespace = re.sub(
         CITATION_REGEX, r"!\g<book> \g<chapter>:\g<verses>", content, flags=re.MULTILINE
     )
-    fixed = fixed_whitespace
-    if fixed == content:
+    if fixed_whitespace == content:
         return
-    with path.open("w") as f:
-        f.write(fixed)
-
+    path.write_text(fixed_whitespace)
 
 def main():
-    a = []
-    for root, dirs, files in os.walk(pathlib.Path(".")):
-        if ".git" in root:
-            continue
-        for f in files:
-            p = pathlib.Path(root, f)
-            if p.suffix in (
+    for path in pathlib.Path(".").rglob("*"):
+        if path.is_file() and path.suffix not in (
                 ".gif",
                 ".ico",
                 ".jpg",
                 ".pdf",
                 ".png",
                 ".ttf",
-            ):
-                continue
-            fix_encoding(p)
-            fix_bible_references(p)
-
+        ):
+            fix_encoding(path)
+            fix_bible_references(path)
 
 if __name__ == "__main__":
     main()
