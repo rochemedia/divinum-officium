89fa5eddde336676c627764e881b3f378515e0b8
Roche Media <robin@rochemedia.ca>
2024-03-07 00:49:22 -0500

    comment

--- web/cgi-bin/horas/Pofficium.pl
+++ web/cgi-bin/horas/Pofficium.pl
@@ -1,3 +1,7 @@
 #!/usr/bin/perl
+
+# Use the FindBin module to get the absolute path of the directory
+# containing this script, and load the officium.pl module located
+# in that directory.
 use FindBin qw($Bin);
 require "$Bin/officium.pl";
